644c1b521bdb4fca4149673c99ca70db77e00283
Natnael Abay <se.natnael.abay@gmail.com>
2023-06-18 19:15:41 +0300

    python magic setup

--- review/forms.py
+++ review/forms.py
@@ -3,7 +3,7 @@ from .models import Feedback
 
 
 class FeedbackForm(forms.ModelForm):
-    pdf_file = forms.FileField()
+    # pdf_file = forms.FileField()
 
     class Meta:
         model = Feedback
@@ -12,5 +12,5 @@ class FeedbackForm(forms.ModelForm):
         widgets = {
             "woreda": forms.TextInput(attrs={"class": "form-control"}),
             "comments": forms.TextInput(attrs={"class": "form-control"}),
-            "pdf_file": forms.TextInput(attrs={"class": "form-control"}),
+            "pdf_file": forms.FileField(attrs={"class": "form-control", "accept" : ".pdf"}),
         }
--- review/models.py
+++ review/models.py
@@ -1,6 +1,19 @@
 import uuid
 from django.db import models
 from user.models import User
+from django.core.validators import FileExtensionValidator
+from django.core.exceptions import ValidationError
+import magic
+
+ext_validator = FileExtensionValidator([".pdf"])
+
+
+def validate_fil_mimetype(file):
+    accept = [".pdf"]
+    file_mime_type = magic.from_buffer(file.read(1024), mime=True)
+    if file_mime_type not in accept:
+        raise ValidationError("Unsupported filetype")
+
 
 class Feedback(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True)
@@ -8,11 +21,16 @@ class Feedback(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     woreda = models.CharField(max_length=500)
     comments = models.TextField()
-    pdf_file = models.FileField(upload_to='feedbacks/', blank=True, null=True)
+    pdf_file = models.FileField(
+        upload_to="feedbacks/",
+        blank=True,
+        null=True,
+        validators=[ext_validator, validate_fil_mimetype],
+    )
     created_at = models.DateTimeField(auto_now_add=True)
 
     is_approved = models.BooleanField(default=False)
-    
+
     def __str__(self):
         return self.user.full_name
 
